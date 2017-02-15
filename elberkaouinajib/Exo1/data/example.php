d1fb5e6a2dd636b5661ad665ad28c09f5afcf3ba
elberkaouinajib <najib.el.berkaoui@gmail.com>
2017-02-15 12:43:03 +0100

    Error fix

--- elberkaouinajib/Exo1/data/example.php
+++ elberkaouinajib/Exo1/data/example.php
@@ -4,7 +4,7 @@
     <script src="http://dimplejs.org/dist/dimple.v2.3.0.min.js"></script>
     <script type="text/javascript">
         var svg = dimple.newSvg("#chartContainer", 590, 400);
-        d3.tsv("example_data.tsv", function (data) {
+        d3.csv("https://docs.google.com/spreadsheets/d/1ZQEgufeLE5I3WyJZ8_bmlVT6c9wRap4L4TAQ_7LRHQw/pub?gid=1596717779&single=true&output=csv", function (data) {
             var myChart = new dimple.chart(svg, data);
             myChart.setBounds(60, 45, 510, 315)
             myChart.addCategoryAxis("x", ["Price Tier", "Channel"]);
