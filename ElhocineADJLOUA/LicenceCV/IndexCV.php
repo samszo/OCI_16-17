5cc566703cdc3fe70759c9c3f273a7f5f997cbe1
ElhocineADJLOUA <elhocine.adjloua@gmail.com>
2017-02-22 12:53:13 +0100

    la liste des CV de tous les étudiants Licence

---
+++ ElhocineADJLOUA/LicenceCV/IndexCV.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+        </title>
+        <style>
+            body{
+                margin: 50px;
+                background-color: blanchedalmond;
+                font-size: 12px;
+                font-weight: bold;
+                  }
+            h1{
+               background-color:graytext;}
+        </style>
+    </head>
+    <body>
+        <!-- la page d'aceuil de mon site -->
+
+        <h1 style="color:lime;text-align: center;font-weight: bold"> Bienvenue sur le site CV des étudiants Licence Professionnel Informatique </h1>  
+    <ul style="margin-top: 50px;margin-bottom 50px;  font-size: 20px;font-weight: bold">  Veuillez choisi quelle étudiant que vous voulez consulter leur CV :
+           
+		   <li style="margin-top: 50px"> <a href="ElhocineADJLOUA.htm">Elhocine ADJLOUA </a> </li>	
+           <li style="margin-top: 15px"> <a href="abdelilahcharai.html">Abd elilah CHARAI </a> </li>
+            <li style="margin-top: 15px"> <a href="achourBRB.html">Achour </a> </li>
+            <li style="margin-top: 15px"> <a href="AdrienPro.html">AdrienPro </a> </li>
+            <li style="margin-top: 15px"> <a href="Chanez.html">Chanez </a> </li>
+            <li style="margin-top: 15px"> <a href="drhouj.html"> drhouj </a> </li>
+            <li style="margin-top: 15px"> <a href="elberkaouinajib.php">elberkaouinajib </a> </li>
+            <li style="margin-top: 15px"> <a href="Mojelloul.html">Mojelloul </a> </li>
+            <li style="margin-top: 15px"> <a href="http://www.metautbenjamin.com"> Happeal </a> </li>
+            <li style="margin-top: 15px"> <a href="http://jthomaseau.com">jeaneudesT </a> </li>
+			<li style="margin-top: 15px"> <a href="nassdjerroud.html">nassdjerroud </a> </li>
+			
+   
+    </ul>   
+
+    </body>
+</html>
+        
